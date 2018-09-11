0abab9b0389bb566001032a7e85dc1abf4231c78
Kayaman Hayri Emrah <hekayaman@hotmail.de>
2018-09-11 16:47:32 +0200

    Add files via upload

---
+++ WebContent/js/medysjs.js
@@ -0,0 +1,9 @@
+$(window).resize(function () { 
+    $('body').css('padding-top', parseInt($('#main-navbar').css("height"))+10);
+});
+
+
+$(document).ready(function()
+{
+	$('body').css('padding-top', parseInt($('#main-navbar').css("height"))+10); 
+});
